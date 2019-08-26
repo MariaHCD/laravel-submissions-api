371a9c787e7d89f05e8852d8c25e935febbeb690
Maria <mariahcdcosta@gmail.com>
2019-08-26 22:18:15 +0400

    Add regex validation for route parameters

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,11 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', ['middleware' => 'bindings'], function ($api) {
     $api->get('submissions', 'App\Http\Controllers\SubmissionController@index');
-    $api->get('submissions/{submission}', 'App\Http\Controllers\SubmissionController@show');
+    $api->get('submissions/{submission}', 'App\Http\Controllers\SubmissionController@show')
+        ->where('submission', '[0-9]+');
     $api->post('submissions', 'App\Http\Controllers\SubmissionController@store');
-    $api->patch('submissions/{submission}', 'App\Http\Controllers\SubmissionController@update');
-    $api->delete('submissions/{submission}', 'App\Http\Controllers\SubmissionController@destroy');
+    $api->patch('submissions/{submission}', 'App\Http\Controllers\SubmissionController@update')
+        ->where('submission', '[0-9]+');
+    $api->delete('submissions/{submission}', 'App\Http\Controllers\SubmissionController@destroy')
+        ->where('submission', '[0-9]+');
 });
